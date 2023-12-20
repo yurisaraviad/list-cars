cbf89c754de75b750ff6bd35a2112bd52ac86108
yurisaraviad <yusaravia@gmail.com>
2023-12-20 10:06:16 -0400

    modal y cerrar con estilos css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from 'axios';
-import { Table, TableContainer, TableHead, TableCell, TableBody, TableRow } from '@mui/material';
+
+
+import { Table, TableContainer, TableHead, TableCell, TableBody, TableRow, Modal, Button, TextField } from '@mui/material';
+import {Edit,Delete} from "@mui/icons-material"
+
+
 
 const baseUrl='http://localhost:3001/cars'
+
+
+
 
 
 
 function App() {
 
   const [data, setData]=useState([]);
+
+  const [modalInsertar, setModalInsertar]=useState(false);
 
    const peticionGet=async()=>{
      await axios.get(baseUrl)
@@ -18,14 +28,55 @@ function App() {
      })
    }
 
+
+   const abrirCerrarModalInsertar=()=>{
+    setModalInsertar(!modalInsertar);
+   }
+
    useEffect(()=>{
      peticionGet();
 
    })
 
+//falta styles
+  const bodyInsertar=(
+    <div className='modal'>
+      <h3>Agregar Nuevo Carro</h3>
+      <TextField label="Id"/>
+      <br />
+      <TextField label="Marca"/>
+      <br />
+      <TextField label="Modelo"/>
+      <br />
+      <TextField label="Year"/>
+      <br />
+      <TextField label="Cilindrada"/>
+      <br />
+      <TextField label="Procedencia"/>
+      <br />
+      <TextField label="Pic1"/>
+      <br />
+      <TextField label="Pic2"/>
+      <br /><br />
+      <div align="right">
+        <Button color="primary">Insertar</Button>
+        <Button onClick={abrirCerrarModalInsertar}>Cancelar</Button>
+      </div>
+      
+
+    </div>
+  )
+
 
   return (
     <div className="App">
+      <br />
+
+      <Button onClick={abrirCerrarModalInsertar}>
+        Insertar
+      </Button>
+
+
       <TableContainer>
         <Table>
           <TableHead>
@@ -39,6 +90,8 @@ function App() {
               <TableCell>Precio</TableCell>
               <TableCell>Pic1</TableCell>
               <TableCell>Pic2</TableCell>
+              <TableCell>Acciones</TableCell>
+              
 
 
             </TableRow>
@@ -46,7 +99,7 @@ function App() {
 
           <TableBody>
             {data.map(cars=>(
-              <TableRow>
+              <TableRow key={cars.id}>
                 <TableCell>{cars.id}</TableCell>
                 <TableCell>{cars.marca}</TableCell>
                 <TableCell>{cars.modelo}</TableCell>
@@ -56,6 +109,11 @@ function App() {
                 <TableCell>{cars.precio}</TableCell>
                 <TableCell>{cars.pic1}</TableCell>
                 <TableCell>{cars.pic2}</TableCell>
+                <TableCell>
+                  <Edit />
+                  &nbsp;&nbsp;&nbsp;
+                  <Delete />
+                </TableCell>
 
               </TableRow>
             ))}
@@ -65,6 +123,15 @@ function App() {
         </Table>
 
       </TableContainer>
+
+
+      <Modal
+      open={modalInsertar}
+      onClose={abrirCerrarModalInsertar}
+      >
+        {bodyInsertar}
+        
+      </Modal>
       
     </div>
   );
